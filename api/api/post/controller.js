6609d5bdc26a074c01110bcc02ff52a1b344308a
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2021-07-31 20:48:02 +0530

    Update

--- api/api/post/controller.js
+++ api/api/post/controller.js
@@ -1,54 +1,87 @@
 import { success, notFound } from '../../services/response/'
 import { Post } from '.'
+const { Validator } = require("encrypted-randomizer");
+const privateKey = "YHGcf9327huwnc38234nef0cv23w40";
 
-export const create = ({ bodymen: { body } }, res, next) => {
+export const create = (req, res, next) => {
+  let {
+    bodymen: { body },
+  } = req;
   const date = new Date();
   const postID = date.getTime();
   const time = date.toUTCString();
+  const decryptChecker = new Validator(privateKey, req.headers.ts);
+  const verificationState = decryptChecker.verifyState(req.headers.erhs);
+  console.log(verificationState);
   body.postID = postID;
   body.time = time;
   body.verified = true;
   body.views = 0;
-  return Post.create(body)
-    // .then((post) => post.view(true))
-    .then(success(res, 201))
-    .catch(next)
-}
+  if (verificationState) {
+    return (
+      Post.create(body)
+        // .then((post) => post.view(true))
+        .then(success(res, 201))
+        .catch(next)
+    );
+  } else {
+    return notFound(res);
+  }
+};
 
-export const index = ({ querymen: { query, select, cursor } }, res, next) =>
-  Post.find(query, select, cursor)
-    .then((posts) => posts.map((post) => post.view()))
-    .then(success(res))
-    .catch(next)
+export const index = (req, res, next) => {
+  let {
+    querymen: { query, select, cursor },
+  } = req;
+  const decryptChecker = new Validator(privateKey, req.headers.ts);
+  const verificationState = decryptChecker.verifyState(req.headers.erhs);
+  console.log(verificationState);
+  if (verificationState) {
+    return Post.find(query, select, cursor)
+      .then((posts) => posts.map((post) => post.view()))
+      .then(success(res))
+      .catch(next);
+    console.log("works");
+  } else {
+    // return notFound(res);
+  }
+  res.send({ done: "done" });
+};
 
-export const show = ({ params }, res, next) =>
-  Post.findOne(params.postID)
-    .then(notFound(res))
-    .then((post) => post ? post.view() : null)
-    .then(success(res))
-    .catch(next)
+export const show = ({ params }, res, next) => {
+  const decryptChecker = new Validator(privateKey, req.headers.TS);
+  const verificationState = decryptChecker.verifyState(req.headers.ERHS);
+  if (verificationState == "true") {
+    return Post.findOne(params.postID)
+      .then(notFound(res))
+      .then((post) => (post ? post.view() : null))
+      .then(success(res))
+      .catch(next);
+  } else {
+    return notFound(res);
+  }
+};
 
 export const update = ({ bodymen: { body }, params }, res, next) =>
   Post.findOne(params.postID)
     .then(notFound(res))
-    .then((post) => post ? Object.assign(post, body).save() : null)
+    .then((post) => (post ? Object.assign(post, body).save() : null))
     // .then((post) => post ? post.view(true) : null)
     .then(success(res))
-    .catch(next)
+    .catch(next);
 
 export const destroy = ({ params }, res, next) =>
   Post.findOne(params.postID)
     .then(notFound(res))
-    .then((post) => post ? post.remove() : null)
+    .then((post) => (post ? post.remove() : null))
     .then(success(res, 204))
-    .catch(next)
+    .catch(next);
 
 export const likeAPost = ({ params }, res, next) => {
   Post.findOne(params.postID)
-  .then(notFound(res))
-  .then((post) => post ? Object.assign(post, body).save() : null)
-  // .then((post) => post ? post.view(true) : null)
-  .then(success(res))
-  .catch(next)
-}
-
+    .then(notFound(res))
+    .then((post) => (post ? Object.assign(post, body).save() : null))
+    // .then((post) => post ? post.view(true) : null)
+    .then(success(res))
+    .catch(next);
+};
